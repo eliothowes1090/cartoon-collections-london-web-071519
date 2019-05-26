db5b6afd2a5962466dbed372b5a44c46606a80fa
Eliot Howes <ejhowes@gmail.com>
2019-05-26 14:04:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(contains_cheddar)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.find {|cheese| cheese_types.each {|cheese_type| cheese_type == cheese}}
 end
